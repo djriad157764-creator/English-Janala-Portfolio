0f6c05cdf04cbf7edea227f4066c0db2e965710c
djriad157764-creator <djriad157764@gmail.com>
2026-03-03 15:35:45 +0600

    add level button with api

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,21 @@
-console.log("i am connected");
-
+// Get ⚡ All Levels
 const lessonsLoad = () => {
-    fetch()
-}
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((data) => displayLessons(data.data));
+};
+
+const displayLessons = (lessons) => {
+  let btnContainer = document.getElementById("btn-container");
+  // btnContainer.innerHTML = "";
+  for (let lesson of lessons) {
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+               <button class="btn btn-outline btn-primary"
+                ><i class="fa-etch fa-solid fa-book-open"></i>Learn ${lesson.level_no}</button
+              >
+    `;
+    btnContainer.appendChild(btnDiv);
+  }
+};
+lessonsLoad();
